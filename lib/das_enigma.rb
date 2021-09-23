640a95972e8072253520eaface5b98335f55cd5c
Stefan Hagen <stefan@ikbenfrits.nl>
2021-09-23 21:20:56 +0200

    Cleaned up top-level module

--- lib/das_enigma.rb
+++ lib/das_enigma.rb
@@ -1,36 +1,9 @@
-require "das_enigma/encryption"
-require "das_enigma/decryption"
-require "das_enigma/enigma"
+# frozen_string_literal: true
 
-class DasEnigma
-
-  def self.help
-    help_text = "Use Das Enigma to encrypt/decrypt .txt files using a simulated Enigma Machine. example: `$ das_enigma encrypt my_file.txt`."
-    puts help_text
+# top-level namespace for the DasEnigma gem
+module DasEnigma
+  # require all ruby files in the lib/das_enigma folder
+  Dir['./lib/das_enigma/**/*.rb'].sort.each do |file|
+    require file
   end
-
-end
-
-ARGV.each do|a| 
-  puts "Argument: #{a}" 
-end
-
-if ARGV[0]
-  if ARGV[0] == "encrypt"
-    if ARGV[1]
-      Encryption.new(ARGV[1])
-    else
-      puts "You need to specify the path to file to be encrypted as the second option."
-    end
-  elsif ARGV[0] == "decrypt"
-    if ARGV[1]
-      Decryption.new(ARGV[1])
-    else
-      puts "You need to specify the path to the encrypted file as the second option."
-    end
-  else 
-    puts "You need to pass the option 'encrypt' or 'decrypt' as the first parameter."
-  end
-else
-  puts "You need to pass the option 'encrypt' or 'decrypt' as the first parameter."
 end
