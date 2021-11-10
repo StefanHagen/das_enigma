db8f6a9e4d4cdd1881c181b5c89ba8e454ca7269
Stefan Hagen <stefan@ikbenfrits.nl>
2021-11-10 23:57:39 +0100

    Added spacing with X character historical convention

--- lib/das_enigma/helpers/input_converter.rb
+++ lib/das_enigma/helpers/input_converter.rb
@@ -8,8 +8,10 @@ module DasEnigma
         @input_data = input_data
       end
 
-      # Upcase the sanitized input for uniformity
+      # Upcase the sanitized input for uniformity, it also uses the historical convention of using 'X' as a spacing
+      # character
       def convert
+        @input_data.gsub!(' ', 'X')
         @input_data.scan(/[a-zA-Z]/).join('').upcase
       end
     end
