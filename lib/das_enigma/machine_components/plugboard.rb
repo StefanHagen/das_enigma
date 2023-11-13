33cf0f49a40167b5143049fa907cfcc807267f66
Stefan Hagen <stefan.hgn@gmail.com>
2023-11-13 21:54:52 +0100

    Small refactor on the plugboard

--- lib/das_enigma/machine_components/plugboard.rb
+++ lib/das_enigma/machine_components/plugboard.rb
@@ -50,10 +50,11 @@ module DasEnigma
       end
 
       def default_plugboard
-        {
-          a: :a, b: :b, c: :c, d: :d, e: :e, f: :f, g: :g, h: :h, i: :i, j: :j, k: :k, l: :l, m: :m, n: :n, o: :o,
-          p: :p, q: :q, r: :r, s: :s, t: :t, u: :u, v: :v, w: :w, x: :x, y: :y, z: :z
-        }
+        {}.tap do |hash|
+          ('a'..'z').to_a.each do |char|
+            hash[char.to_sym] = char.to_sym
+          end
+        end
       end
     end
   end
